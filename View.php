ae0c5856c2e86548356cfd59ac849a4a6202dec8
Artem Zazymko <artemzazimko@gmail.com>
2017-09-14 10:51:41 +0300

    first commit

---
+++ View.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace zazymko\swiftmailer;
+
+/**
+ * @author Dmitriy zazymko <dmitriy@zazymko.com>
+ */
+class View
+{
+
+    /**
+     * @param $view
+     * @param array $params
+     * @param null $context
+     * @return string
+     */
+    public function render($view, $params = [], $context = null)
+    {
+        $viewFile = $context->getViewPath() . '/' . $view . '.php';
+        return $this->renderPhpFile($viewFile, $params, $context);
+    }
+
+    /**
+     * Renders a view file as a PHP script.
+     * @param string $_file_ the view file.
+     * @param array $_params_ the parameters (name-value pairs) that will be extracted and made available in the view file.
+     * @return string the rendering result
+     */
+    public function renderPhpFile($_file_, $_params_ = [])
+    {
+        ob_start();
+        ob_implicit_flush(false);
+        extract($_params_, EXTR_OVERWRITE);
+        require($_file_);
+        return ob_get_clean();
+    }
+
+}
